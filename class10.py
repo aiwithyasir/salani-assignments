0e368cff30918d2152d151436c823a7eb8ec9da0
yasir <yasirpubglover12@gmail.com>
2025-04-27 12:39:43 +0500

    task complete and selilium paractice

---
+++ class10.py
@@ -0,0 +1,12 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+
+website = 'http://adamchoi.co.uk/leagues/spain-la-liga'
+path = r'C:\chromedriver-win64\chromedriver.exe'
+service = Service(path)
+service.start()
+
+driver = webdriver.Chrome(path)
+driver.get(website)
+
+driver.find_element_by_xpath("//a[@data-ng-click=\"vm.setTabType('Top Players')\"]").click()
